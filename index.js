42ee1c51b0631b56f513f3e03d76dc4eef096529
Nirupama <nirupamaranjan2414@gmail.com>
2022-01-22 21:07:23 +0530

    complete task

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 
 function playAudio(e){
-  console.log(e);
-}
+  const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key=document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if(!audio) return  //this part is error to prevent typeerror
+  key.classList.add('playing')
+  audio.currentTime=0  //to rewind to start
+  audio.play()
+  }
+
+  function removeTransition(e){
+      if(e.propertyName!=='transform') return
+      this.classList.remove("playing")
+  }
 
 window.addEventListener("keydown",playAudio)
+
+const keys=document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener("transitionend",removeTransition))
